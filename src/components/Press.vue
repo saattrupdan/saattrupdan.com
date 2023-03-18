479a9fb1eb75ccf5a6019f7402544eba724490f8
saattrupdan <saattrupdan@gmail.com>
2023-03-18 17:35:35 +0100

    style: Make press articles a bit nicer, adds shadow

--- src/components/Press.vue
+++ src/components/Press.vue
@@ -6,20 +6,30 @@
   <div class="centered-box">
 
     <a href="https://pro.ing.dk/datatech/holdning/dansk-data-science-community-open-source">
-      <img src="/2022-05-10-open-source.png" alt="Open source article"/>
+      <img src="/2022-05-10-open-source.png" alt="Open source article" class="article"/>
     </a>
     <a href="https://pro.ing.dk/datatech/artikel/forsker-bag-nyt-benchmark-den-gaengse-maade-evaluere-sprogmodeller-paa-i-dag-er">
-      <img src="/2021-10-28-scandeval.png" alt="ScandEval article"/>
+      <img src="/2021-10-28-scandeval.png" alt="ScandEval article" class="article"/>
     </a>
     <a href="https://pro.ing.dk/datatech/artikel/ny-sprogmodel-er-fintunet-paa-fem-skandinaviske-sprog-og-det-fungerer-overraskende">
-      <img src="/2021-10-12-scandiner.png" alt="ScandiNER article"/>
+      <img src="/2021-10-12-scandiner.png" alt="ScandiNER article" class="article"/>
     </a>
     <a href="https://pro.ing.dk/datatech/artikel/data-scientists-paa-tvaers-af-brancher-brugte-ferie-paa-lave-ny-dansk-sprogmodel">
-      <img src="/2021-08-10-roebaerta.png" alt="RøBÆRTa article"/>
+      <img src="/2021-08-10-roebaerta.png" alt="RøBÆRTa article" class="article"/>
     </a>
 
   </div>
 </template>
 
 <style scoped>
+  .article {
+    transition: 0.3s;
+    width: 80%;
+    display: block;
+    margin: auto auto 10px auto;
+  }
+
+  .article:hover {
+    box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
 </style>
