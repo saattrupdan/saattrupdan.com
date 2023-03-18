f68ef6f0781de6c70120180e5b67bbe589470946
saattrupdan <saattrupdan@gmail.com>
2023-03-18 17:35:57 +0100

    chore: Move IDA webinar, add comments

--- src/components/Podcasts.vue
+++ src/components/Podcasts.vue
@@ -5,13 +5,17 @@
   <h1 class="centered">Podcasts and Webinars</h1>
   <div class="centered-box">
 
-    <iframe src="https://open.spotify.com/embed/episode/0vrF4vDknH7UWc8uIUtSd1?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
-    <iframe src="https://open.spotify.com/embed/episode/6Ne1WJweO4KXcc0ujuVOfL?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
+    <!-- IDA webinar -->
     <div class="webinar">
       <iframe id="kaltura_player" src="https://cdnapisec.kaltura.com/p/3211243/sp/321124300/embedIframeJs/uiconf_id/49568992/partner_id/3211243?iframeembed=true&playerId=kaltura_player&entry_id=1_fi689bm6&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=da&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[hotspots.plugin]=1&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_7lffdg9l" width="608" height="402" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Generativ AI i praksis" style="position:absolute;top:0;left:0;width:100%;height:100%"></iframe>
     </div>
+
+    <!-- Verbos podcast -->
+    <iframe src="https://open.spotify.com/embed/episode/0vrF4vDknH7UWc8uIUtSd1?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+    <!-- EDB 5.0 podcast -->
+    <iframe src="https://open.spotify.com/embed/episode/6Ne1WJweO4KXcc0ujuVOfL?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
   </div>
 </template>
 
