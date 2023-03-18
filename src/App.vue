472c0cf90b1d2e8ec0adc5121f13a99dbf595164
saattrupdan <saattrupdan@gmail.com>
2023-03-18 17:35:17 +0100

    feat: Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,78 @@
 </script>
 
 <template>
-  <div class="topnav sans-serif-text">
-    <div class="topnav-brand">
-      <router-link to="/">Dan Saattrup Nielsen</router-link>
-    </div>
-    <div class="topnav-links">
+  <div class="header-footer topnav sans-serif-text">
+
+    <!-- Navigation bar -->
+    <div class="right">
       <router-link to="/press">Press</router-link>
       <router-link to="/podcasts">Podcasts and Webinars</router-link>
       <router-link to="/projects">Personal Projects</router-link>
       <router-link to="/papers">Papers</router-link>
-      <router-link to="/">About Me</router-link>
       <router-link to="/posts">Blog</router-link>
+      <router-link to="/">About Me</router-link>
     </div>
     <img src="/avatar-icon.png" alt="avatar" class="avatar"/>
   </div>
 
   <!-- Render the current view !-->
-  <div class="top-margin">
+  <div class="margins">
     <router-view />
   </div>
+
+  <!-- Footer -->
+  <div class="header-footer footer sans-serif-text">
+    <p class="social-icons">
+      <a href="https://github.com/saattrupdan">
+        <img src="/github-icon.png" alt="github" class="footer-icon"/>
+      </a>
+      <a href="https://www.linkedin.com/in/saattrupdan/">
+        <img src="/linkedin-icon.png" alt="linkedin" class="footer-icon"/>
+      </a>
+      <a href="https://scholar.google.com/citations?user=aNojQDEAAAAJ&hl=en">
+        <img src="/scholar-icon.png" alt="scholar" class="footer-icon"/>
+      </a>
+    </p>
+    <p class="copyright">
+      <p>© 2023 Dan Saattrup Nielsen</p>
+    </p>
+  </div>
+
 </template>
 
 <style scoped>
 
-  /* Add a black background color to the top navigation */
-  .topnav {
+  .footer-icon {
+    width: 30px;
+    height: 30px;
+    margin: 0 8px;
+    transition: 0.3s;
+  }
+
+  .footer-icon:hover {
+    opacity: 0.6;
+  }
+
+  .header-footer {
     background-color: #F5F5F5;
     overflow: hidden;
-    padding: 0 5%;
-    margin-bottom: 20px;
+    padding: 0px 1%;
     position: fixed;
     left: 0;
     right: 0;
+  }
+
+  .footer {
+    bottom: 0;
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .topnav {
+    margin-bottom: 20px;
     top: 0;
   }
 
-  /* Style the links inside the navigation bar */
   .topnav a {
     display: block;
     color: #3b3c36;
@@ -45,29 +81,24 @@
     padding: 25px 16px;
     text-decoration: none;
     line-height: 20px;
-    font-weight: 800;
+    transition: 0.3s;
   }
 
-  /* Change the color of links on hover */
   .topnav a:hover {
     color: #0085A1;
+    font-size: 13px;
   }
 
-  .topnav-brand a {
-    float: left;
-    font-size: 1.1rem;
-  }
-
-  .topnav-links a {
+  .right a {
     float: right;
-    font-size: 0.75rem;
+    font-size: 12px;
     text-transform: uppercase;
-    font-weight: 800;
     letter-spacing: 1px;
   }
 
-  .top-margin {
-    margin-top: 120px;
+  .margins {
+    margin-top: 90px;
+    margin-bottom: 150px;
   }
 
   .avatar {
