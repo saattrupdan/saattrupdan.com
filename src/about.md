6d078b1a25171ad8c9c15efa0ba8cbbc63c37dc4
saattrupdan <saattrupdan@gmail.com>
2023-03-19 15:45:42 +0100

    feat: Better about page

---
+++ src/about.md
@@ -0,0 +1,15 @@
+Welcome to my website! 👋
+
+I'm currently working at the Alexandra Institute as a Senior AI Specialist,
+where I'm spending half my time conducting research and the other half
+helping out Danish companies with their machine learning needs.
+
+I have an academic background, with a Ph.D. in Mathematics and a Postdoc in
+Machine Learning, and also several years of industry experience in the public
+and private sector.
+
+I'm really interested in low-resource natural language processing
+(especially for Scandinavian languages), bias in language models, uncertainty
+estimation methods, and graph machine learning.
+
+Check out [my CV]() for more details.
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,25 +1,64 @@
 <script setup>
+  import { ref } from 'vue'
   import Greeting from './Greeting.vue'
+  import Description from '../about.md'
+  const photoUrl = "/itu-photo.jpg"
+  const wrappedPhotoUrl = `url(${photoUrl})`
 </script>
 
 <template>
   <Greeting/>
-  <div class="centered-box">
-    <div class="serif-text">
-
-      Senior AI Specialist with highly specialised knowledge of machine learning methods.
-      My PhD in Mathematics has given me the technical knowledge to tweak and invent
-      novel machine learning algorithms to solve new problems, and my industrial
-      experience has given me the skills to implement these algorithms in
-      production-level code. My primary research interests are within natural language
-      processing and graph machine learning.
-
+  <div class="container">
+    <div class="photo-box">
+      <div class="photo-croppable"></div>
+      <img class="photo" :src="photoUrl"/>
+    </div>
+    <div class="description-box">
+      <div class="serif-text description">
+        <Description/>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .centered {
-    text-align: center;
+  .container {
+    display: flex;
+    justify-content: center;
+  }
+  .photo-box {
+    flex: 1 1 0px;
+  }
+  .photo {
+    visibility: hidden;
+    display: none;
+  }
+  .photo-croppable {
+    height: 100%;
+    background-image: v-bind("wrappedPhotoUrl");
+    background-position: right;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .description-box {
+    flex: 1 1 0px;
+    background: rgb(228 224 224);
+  }
+  .description-box >>> * {
+    background-color: transparent;
+  }
+  .description {
+    margin: 5% 10% 10% 10%;
+  }
+  @media (max-width: 900px) {
+    .container {
+      flex-wrap: wrap;
+    }
+    .photo-box {
+      flex-basis: 100%;
+    }
+    .photo {
+      display: block;
+    }
   }
 </style>
