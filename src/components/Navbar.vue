840cf957a5f3169eea1407e0aa539f785f965b4d
saattrupdan <saattrupdan@gmail.com>
2023-03-21 22:06:05 +0100

    feat: Add hamburger navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,60 @@
 <script setup>
+  import { ref } from 'vue'
+  const hamburgerMenuVisible = ref(false);
 </script>
 
 <template>
-  <div class="navbar">
-    <router-link to="/press">Press</router-link>
-    <router-link to="/podcasts">Podcasts</router-link>
-    <router-link to="/projects">Projects</router-link>
-    <router-link to="/papers">Papers</router-link>
-    <router-link to="/posts">Blog</router-link>
-    <router-link to="/">About Me</router-link>
-  </div>
+  <nav class="navbar" id="navbar">
+    <div :class="hamburgerMenuVisible ? 'nav-menu active' : 'nav-menu'">
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/">
+        About Me
+      </router-link>
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/posts">
+        Blog
+      </router-link>
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/papers">
+        Papers
+      </router-link>
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/projects">
+        Projects
+      </router-link>
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/podcasts">
+        Podcasts
+      </router-link>
+      <router-link class="nav-item" @click.native="hamburgerMenuVisible = false" to="/press">
+        Press
+      </router-link>
+    </div>
+    <div
+      :class="hamburgerMenuVisible ? 'hamburger active' : 'hamburger'"
+      @click="hamburgerMenuVisible = !hamburgerMenuVisible"
+    >
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
   .navbar {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    padding: 1.5rem 1.5rem;
+  }
+  .nav-menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
   .navbar a {
-    display: block;
-    float: right;
     font-size: 14px;
     text-transform: uppercase;
     letter-spacing: 1px;
     color: #3b3c36;
     text-align: center;
-    padding: 25px 16px;
+    margin-left: 2rem;
     text-decoration: none;
     line-height: 20px;
     transition: 0.3s;
@@ -31,5 +62,53 @@
   .navbar a:hover {
     color: #0085A1;
     font-size: 15px;
+  }
+  .nav-item {
+    margin-left: 5rem;
+  }
+  .hamburger {
+    display: none;
+  }
+  .bar {
+    display: block;
+    width: 25px;
+    height: 3px;
+    margin: 5px auto;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    background-color: #101010;
+  }
+  @media only screen and (max-width: 768px) {
+    .nav-menu {
+      position: fixed;
+      left: 0;
+      top: -100%;
+      flex-direction: column;
+      background-color: #fff;
+      width: 100%;
+      border-radius: 10px;
+      text-align: center;
+      transition: 0.3s;
+      box-shadow: 0 10px 27px rgba(0, 0, 0, 0.05);
+    }
+    .nav-item {
+      margin: 0.5rem 0;
+    }
+    .nav-menu.active {
+      top: 4.0rem;
+    }
+    .hamburger {
+      display: block;
+      cursor: pointer;
+    }
+    .hamburger.active .bar:nth-child(2) {
+      opacity: 0;
+    }
+    .hamburger.active .bar:nth-child(1) {
+      transform: translateY(8px) rotate(45deg);
+    }
+    .hamburger.active .bar:nth-child(3) {
+      transform: translateY(-8px) rotate(-45deg);
+    }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,3 @@ import { router } from './router.js'
 import './assets/main.css'
 
 createApp(App).use(router).mount('#app')
-app.use(router)
-app.mount('#app')
