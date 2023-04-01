36a6da5604fb2925b172b741894ab9aa9b8358e4
saattrupdan <saattrupdan@gmail.com>
2023-04-01 14:45:11 +0200

    style: Reordering imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import { router } from './router.js'
+import App from './App.vue'
+
 import './assets/main.css'
 
 createApp(App).use(router).mount('#app')
