b10f4e7e194f5d3eab9925e96027fdec86d692eb
saattrupdan <saattrupdan@gmail.com>
2023-04-02 16:59:42 +0200

    fix: Set meta descriptions properly for posts

--- src/router.js
+++ src/router.js
@@ -100,11 +100,24 @@ const router = createRouter({
 // This ensures that the page title is updated when the route changes, assuming that
 // the page title has been set in the route's meta data.
 router.beforeEach((to, from, next) => {
-  const title = to.meta.title
-  const description = to.meta.description
-  if (title) {
-    document.title = title
-    document.querySelector('meta[name="description"]').setAttribute('content', description)
+  const id = to.params.id
+  if (id) {
+    import(`@/posts/${id}.md`).then((module) => {
+      const title = module.frontmatter['title']
+      const description = module.frontmatter['meta']
+      document.title = title
+      document.querySelector('meta[name="description"]').setAttribute('content', description)
+    })
+  }
+  else {
+    const title = to.meta.title
+    const description = to.meta.description
+    if (title) {
+      document.title = title
+    }
+    if (description) {
+      document.querySelector('meta[name="description"]').setAttribute('content', description)
+    }
   }
   next()
 })
