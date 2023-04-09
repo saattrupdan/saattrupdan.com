2b54f7832c2cff91104fbaed613e58331608741a
saattrupdan <saattrupdan@gmail.com>
2023-04-09 16:24:21 +0200

    refactor: Move CSS variables to separate YAML file

--- src/stores/darkmode.js
+++ src/stores/darkmode.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import cssVariables from '@/assets/css-variables.yaml'
 import { defineStore } from 'pinia'
 
 export const useDarkModeStore = defineStore('darkmode', () => {
@@ -18,36 +19,16 @@ export const useDarkModeStore = defineStore('darkmode', () => {
 
   function enableDarkMode() {
     darkmode.value = true
-    root.style.setProperty('--bg-main', '#2d2d2d')
-    root.style.setProperty('--bg-header-footer', '#393939')
-    root.style.setProperty('--bg-box', '#393939')
-    root.style.setProperty('--bg-table', '#3d3d3d')
-    root.style.setProperty('--bg-code-1', '#3a3a3a')
-    root.style.setProperty('--bg-code-2', '#3d3d3d')
-    root.style.setProperty('--bg-dark-mode-toggle', '#777777')
-    root.style.setProperty('--title-color', '#dedede')
-    root.style.setProperty('--text-color', '#dedede')
-    root.style.setProperty('--link-color', '#dedede')
-    root.style.setProperty('--quote-color', '#c0c0c0')
-    root.style.setProperty('--image-brightness', '80%')
-    root.style.setProperty('--image-inverted', '100%')
+    for (const [key, value] of Object.entries(cssVariables.darkmode)) {
+      root.style.setProperty(`--${key}`, String(value))
+    }
   }
 
   function disableDarkMode() {
     darkmode.value = false
-    root.style.setProperty('--bg-main', '#F5F4F2')
-    root.style.setProperty('--bg-header-footer', 'white')
-    root.style.setProperty('--bg-box', '#e4e0e0')
-    root.style.setProperty('--bg-table', '#f2f2f2')
-    root.style.setProperty('--bg-code-1', '#e3e3e3')
-    root.style.setProperty('--bg-code-2', '#ececec')
-    root.style.setProperty('--bg-dark-mode-toggle', '#bcbcbc')
-    root.style.setProperty('--title-color', '#3b3c36')
-    root.style.setProperty('--text-color', '#3b3c36')
-    root.style.setProperty('--link-color', '#3b3c36')
-    root.style.setProperty('--quote-color', '#808080')
-    root.style.setProperty('--image-brightness', '100%')
-    root.style.setProperty('--image-inverted', '0%')
+    for (const [key, value] of Object.entries(cssVariables.lightmode)) {
+      root.style.setProperty(`--${key}`, String(value))
+    }
   }
 
   function toggleDarkMode() {
