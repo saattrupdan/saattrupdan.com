daf7e8488d6942451ac7401ca2a2301e57301f60
saattrupdan <saattrupdan@gmail.com>
2025-11-10 22:17:37 +0100

    feat: Add demo down message

--- src/frontend/views/GeomaticDemoView.vue
+++ src/frontend/views/GeomaticDemoView.vue
@@ -1,12 +1,19 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="geomatic-demo-container">
+  <div class="geomatic-demo-container sans-serif-text">
+    <div class="demo-down-message">
+      Demoen er desværre nede for i dag, da serveren er reserveret til en
+      workshop. Prøv igen i morgen.
+    </div>
+
+    <!--
     <iframe
       src="https://15292cf1f7876a0331.gradio.live/"
       width="100%"
       height="100%"
       frameborder="0"
     />
+    -->
   </div>
 </template>
 <style scoped>
@@ -20,5 +27,11 @@
 .geomatic-demo-container iframe {
   display: block;
   border: none;
+}
+.demo-down-message {
+  font-size: 1.5rem;
+  color: var(--text-color);
+  text-align: center;
+  margin-top: 20%;
 }
 </style>
