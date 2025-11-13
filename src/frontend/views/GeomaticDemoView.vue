01f748126eb94084c2793b210661641ae23bc764
saattrupdan <saattrupdan@gmail.com>
2025-11-13 10:19:27 +0100

    feat: Make Geomatic demo live again

--- src/frontend/views/GeomaticDemoView.vue
+++ src/frontend/views/GeomaticDemoView.vue
@@ -1,19 +1,19 @@
 <script lang="ts" setup></script>
 <template>
   <div class="geomatic-demo-container sans-serif-text">
+    <!--
     <div class="demo-down-message">
       Demoen er desværre nede for i dag, da serveren er reserveret til en
       workshop. Prøv igen i morgen.
     </div>
+    -->
 
-    <!--
     <iframe
-      src="https://15292cf1f7876a0331.gradio.live/"
+      src="https://a2adc98bd3c28df8a8.gradio.live/"
       width="100%"
       height="100%"
       frameborder="0"
     />
-    -->
   </div>
 </template>
 <style scoped>
