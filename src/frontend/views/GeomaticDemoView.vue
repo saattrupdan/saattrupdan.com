7ca8bd5e02a13428a3e119cc0421f82d7a08903d
saattrupdan <saattrupdan@gmail.com>
2025-11-27 14:22:14 +0100

    chore: Update Geomatic

--- src/frontend/views/GeomaticDemoView.vue
+++ src/frontend/views/GeomaticDemoView.vue
@@ -8,7 +8,7 @@
     </div>
     -->
     <iframe
-      src="https://0a54459431d228d795.gradio.live/"
+      src="https://2945febe07399c0f88.gradio.live/"
       width="100%"
       height="100%"
       frameborder="0"
