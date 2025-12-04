956232a4d4157cd0f928f81f8f6e4f8fb041861a
saattrupdan <saattrupdan@gmail.com>
2025-12-04 16:16:24 +0100

    chore: Restart Geomatic server

--- src/frontend/views/GeomaticDemoView.vue
+++ src/frontend/views/GeomaticDemoView.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup></script>
 <template>
   <div class="geomatic-demo-container sans-serif-text">
+    <!--
     <div class="demo-down-message">
       Demoen er desværre nede for i dag, da serveren er reserveret til en
       workshop. Prøv igen i morgen.
     </div>
-    <!--
+    -->
     <iframe
       src="https://ec961ac235f1ddc41a.gradio.live/"
       width="100%"
       height="100%"
       frameborder="0"
     />
-    -->
   </div>
 </template>
 <style scoped>
