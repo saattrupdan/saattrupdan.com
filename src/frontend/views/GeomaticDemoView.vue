30ae9ee76450b6e8fb2accd9e4d6c5f7014e7caf
saattrupdan <saattrupdan@gmail.com>
2026-01-20 16:53:01 +0100

    feat: Update GeomaticDemoView

--- src/frontend/views/GeomaticDemoView.vue
+++ src/frontend/views/GeomaticDemoView.vue
@@ -6,6 +6,9 @@ const demoDownDates = [
   new Date("2025-12-11"),
   new Date("2026-01-08"),
   new Date("2026-01-15"),
+  new Date("2026-01-27"),
+  new Date("2026-01-28"),
+  new Date("2026-02-03"),
 ];
 const isDemoDown = ref(
   demoDownDates.some(
@@ -15,10 +18,23 @@ const isDemoDown = ref(
       date.getFullYear() === today.getFullYear(),
   ),
 );
+const isDemoDownTomorrow = ref(
+  demoDownDates.some(
+    (date) =>
+      date.getDate() === today.getDate() + 1 &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear(),
+  ),
+);
 </script>
 <template>
   <div class="geomatic-demo-container sans-serif-text">
-    <div class="demo-down-message" v-if="isDemoDown">
+    <div class="demo-down-message" v-if="isDemoDown && isDemoDownTomorrow">
+      Demoen er desværre nede for i dag
+      {{ today.toLocaleDateString("da-DK") }} og i morgen, da serveren er
+      reserveret til en workshop. Demoen vil være tilgængelig igen i overmorgen.
+    </div>
+    <div class="demo-down-message" v-else-if="isDemoDown">
       Demoen er desværre nede for i dag {{ today.toLocaleDateString("da-DK") }},
       da serveren er reserveret til en workshop. Demoen vil være tilgængelig
       igen i morgen.
